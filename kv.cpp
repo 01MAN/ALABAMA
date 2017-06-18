bbebef40189ee486b415a858c9d8fb066fe3a34d
Your Name <you@example.com>
2017-06-18 23:39:35 +0300

    Include

--- kv.cpp
+++ kv.cpp
@@ -2,4 +2,27 @@
 #include <math.h>
 main()
 {
+    float a,b,c;
+    printf("vvedite a, b, c: ");
+    scanf("%f %f %f", &a, &b, &c);
+    if(a == 0)
+    {
+        if(b == 0)
+        {
+            if(c == 0)
+            {
+                printf("x - lyboe 4islo");
+            }
+            else
+            {
+                printf("kornei net");
+            }
+        }
+        else
+        {
+            printf ("x = %g", -c/b);
+        }
+    }
     
+    return 0;
+}
